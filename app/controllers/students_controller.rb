2c5d820409c00093567b769bc4b7e9ebd489de62
Sai Prasad Sappati <bunny.sd62@gmail.com>
2023-02-21 10:08:32 +0530

    The all CRUD operations have been performed by hand

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,44 @@
 class StudentsController < ApplicationController
 	def show
-		# byebug
 		@student=Student.find(params[:id])
 	end
 
 	def index
 		@students=Student.all
+	end
+
+	def new
+		@student=Student.new
+	end
+
+	def edit
+		@student=Student.find(params[:id])
+	end
+
+	def create
+		# render plain: params[:student]
+		@student=Student.new(params.require(:student).permit(:first_name, :last_name))
+		if @student.save
+			flash[:notice]="Student was successfully created."
+			redirect_to student_path(@student)
+		else
+			render 'new'
+		end
+	end
+
+	def update
+		@student=Student.find(params[:id])
+		if @student.update(params.require(:student).permit(:first_name, :last_name))
+			flash[:notice]="Student was successfully updated."
+			redirect_to @student
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@student=Student.find(params[:id])
+		@student.destroy
+		redirect_to students_path
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 	root 'pages#home'
 	get 'form', to: 'pages#form'
-	resources :students, only: [:show, :index]
+	resources :students, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
