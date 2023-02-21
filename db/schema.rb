6c68cd0192a5dd06d85e763406659355b558f870
Sai Prasad Sappati <bunny.sd62@gmail.com>
2023-02-21 15:36:59 +0530

    created user and user_model with validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_14_100654) do
+ActiveRecord::Schema.define(version: 2023_02_21_074541) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 2023_02_14_100654) do
     t.string "last_name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
